5c549348eadbb239e283e2f6d3f7d2b7820a560c
HoangSonVu2301 <89793132+HoangSonVu2301@users.noreply.github.com>
2022-01-15 17:31:05 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if(this.scrollY > 20){
-            $('.nav_bar').addClass("sticky");
-        }else{
-            $('.nav_bar').removeClass("sticky");
-        }
-    })
-});
